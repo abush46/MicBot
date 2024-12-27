990f356c27129f223f5c18b7be10dfce3ff51e07
Michael Samson <40731418+abush46@users.noreply.github.com>
2024-12-27 19:32:31 +0300

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -25,9 +25,8 @@ try{
   return ctx.reply(
     'random example',
     Markup.inlineKeyboard([
-      Markup.button.callback('Coke', 'Coke'),
-      Markup.button.callback('Dr Pepper', 'Dr Pepper', Math.random() > 0.5),
-      Markup.button.callback('Pepsi', 'Pepsi')
+      Markup.button.url('LAUNCH', 'http://t.me/MicSoftware_bot/MicApp')
+      
     ])
   )
 } 
